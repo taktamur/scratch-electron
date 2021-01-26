1be977441a8d6846e224cc91e4239768faf244b8
Takafumi Tamura <taktamur@gmail.com>
2021-01-26 20:43:58 +0900

    update

--- ipc-ts/src/main.ts
+++ ipc-ts/src/main.ts
@@ -18,7 +18,7 @@ function createWindow() {
   });
 
   // and load the index.html of the app.
-  mainWindow.loadFile("src/index.html");
+  mainWindow.loadFile("index.html");
 
   // Open the DevTools.
   mainWindow.webContents.openDevTools();
